a14e2236d42f54521407fa2a42046958c7f012fa
richinrix <richinvishal@gmail.com>
2022-03-28 12:52:52 +0530

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,33 @@
 import Head from 'next/head';
+import HomepageCipherList from '../sections/HomepageCipherList';
 import { MdOpenInNew } from 'react-icons/md';
 import { GoMarkGithub } from 'react-icons/go';
+const jokes = [
+  'Two hashes walk into a bar, one was a salted.',
+  'What do Vikings use to encrypt their messages? \n  Norse code',
+  'Yo mama is so used, they call her IPv4.',
+];
 const Home = () => {
   return (
-    <div className=" flex   flex-col items-center  justify-center   lg:h-90vh">
+    <div className=" flex   flex-col items-center  justify-center   ">
       <Head>
         <title>Land of Ciphers</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div class="flex w-full flex-col  items-center px-3 lg:flex-row lg:px-20 ">
+      <div class="flex w-full flex-col  items-center px-3 lg:h-90vh lg:flex-row lg:px-20 ">
         <div class="hidden h-full w-1/4 flex-col items-center justify-center border-r-1 border-brandDarkGreen px-10 py-10 lg:flex ">
-          <div class="text-sm text-gray-500">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam,
-            nesciunt?
+          <div class="w-full text-left text-sm text-gray-500">
+            {jokes[Math.floor(Math.random() * jokes.length)]}
           </div>
-          <div class="my-10 h-64 w-full rounded-md bg-brandLightBlue"></div>
+          <div
+            style={{
+              backgroundImage: `url('/images/homepage.jpg')`,
+            }}
+            class="my-10 h-64 w-full rounded-md bg-brandLightBlue bg-cover bg-center"
+          ></div>
         </div>
-        <div class=" h-full w-full  py-10 lg:w-3/4 lg:px-10 ">
+        <div class="flex h-full w-full flex-col justify-between  py-10 lg:w-3/4 lg:px-10 ">
           <div class=" font-montserrat text-5xl font-bold lg:text-8xl ">
             <span class="text-brandPink">Welcome </span>
             <span class="text-gray-800">
@@ -34,7 +44,7 @@ const Home = () => {
                 class=" flex  h-full w-full cursor-pointer items-center  justify-center  rounded-lg border-2 border-brandLightBlue px-10 py-5 hover:bg-brandLightBlue"
               >
                 <div class="h-full w-2/3">
-                  <div className="my-2 text-xl font-semibold text-gray-600  ">
+                  <div className="mb-2 text-xl font-semibold text-gray-600  ">
                     Checkout the ciphers
                   </div>
                   <div class=" mx-auto rounded-md  bg-brandDarkGreen px-5 py-1 text-center text-xl uppercase text-white ">
@@ -54,7 +64,7 @@ const Home = () => {
                 class=" flex h-full w-full cursor-pointer items-center  justify-center  rounded-lg border-2 border-brandLightBlue px-10 py-5 hover:bg-brandLightBlue"
               >
                 <div class="h-full w-2/3">
-                  <div className="my-2 text-xl font-semibold text-gray-600  ">
+                  <div className="mb-2 text-xl font-semibold text-gray-600  ">
                     Collab with us
                   </div>
                   <div class=" mx-auto rounded-md  bg-brandDarkGreen px-5 py-1 text-center text-xl uppercase text-white ">
@@ -69,6 +79,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <HomepageCipherList />
     </div>
   );
 };
---
+++ sections/HomepageCipherList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomepageCipherList = () => {
+  return <div>HomepageCipherList</div>;
+};
+
+export default HomepageCipherList;
