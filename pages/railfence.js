fba06e2072efe19440580f1a56cf37861b5d3a10
Marshyy <aniketh470@gmail.com>
2022-12-12 20:20:15 +0530

    Railfence and Vigenere Ciphers added

---
+++ pages/railfence.js
@@ -0,0 +1,163 @@
+import React, { useState } from 'react';
+
+const railfence = () => {
+  const [plainText, setPlainText] = useState('');
+  const [cipherText, setCipherText] = useState('');
+  const [railValue, setRailValue] = useState(3);
+
+  function RailFence(message) {
+    if (typeof railValue != "number" || railValue < 2) setRailValue(3);
+  
+    let fence = []; 
+    let direction = 0; // 0 is down, 1 is up
+    let num = 0;
+    
+    for (let i=0; i < railValue; i++) fence.push([]);
+    
+    for (let i=0; i < message.length; i++) {
+        let m = message[i];
+        
+        if (direction == 0) {
+            fence[num].push(m); num++;
+            if (num == railValue) {
+                direction = 1;
+                num = num-2;
+            }
+        } else {
+            fence[num].push(m); num--;
+            if (num == -1) {
+                direction = 0;
+                num = num+2;
+            }
+        }
+    }
+    
+    let result = "";
+
+    for (let i = 0; i < railValue; i++) {
+        result += fence[i].join("");
+    }
+    
+    return result;
+  }
+
+  function decryptor(message) {
+    let parts = [];
+    let k = Math.round(message.length/(2*(railValue-1)));
+
+    parts[0] = message.slice(0, k);
+    parts[1] = message.slice(0+k, message.length-k);
+    parts[2] = message.slice(-k);
+
+    let result = "";
+
+    for (let i=0 ; i < parts[0].length; i++) {
+      result += parts[0].charAt(i);
+      result += parts[1].charAt(2*i);
+      result += parts[2].charAt(i);
+      result += parts[1].charAt(2*i+1);
+    }
+
+    return result.slice(0, message.length);
+  }
+
+  return (
+    <div className="min-h-screen px-3 py-5 lg:px-20 lg:py-10">
+      <div class="w-full text-center text-6xl font-bold text-brandPink">
+        RailFence Cipher
+      </div>
+      <div className="my-3 w-full text-left text-sm text-gray-600">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum
+        praesentium aliquam illum consequatur vitae inventore distinctio hic
+        nisi, sint molestias quam dignissimos et! Rem praesentium, soluta ipsum,
+        explicabo temporibus officiis iste minima odio iusto, quidem
+        necessitatibus quas. Quidem, ipsa nostrum!
+      </div>
+      <div class="flex w-full">
+        <div class=" w-1/2 p-5">
+          <div class="font-sourceCodepro text-xl text-gray-500">Plain Text</div>
+          <textarea
+            value={plainText}
+            onChange={(e) => {
+              setPlainText(e.target.value);
+            }}
+            placeholder="Enter text here"
+            className="my-3  w-full bg-brandDarkGray p-2  outline-none placeholder:text-gray-600"
+            rows="10"
+          ></textarea>
+          <button
+            className="mr-5 rounded-md bg-brandPink px-3 py-2 text-white "
+            onClick={() => {
+              setCipherText(RailFence(plainText));
+            }}
+          >
+            Encrypt
+          </button>
+          <button
+            className="rounded-md bg-brandPink px-3 py-2 text-white"
+            onClick={() => {
+              setPlainText('');
+            }}
+          >
+            Clear Encrypt
+          </button>
+        </div>
+
+        <div class=" w-1/2 p-5">
+          <div class="font-sourceCodepro text-xl text-gray-500">Rail Value</div>
+          <textarea
+            // value={secretKey}
+            onChange={(e) => {
+              setRailValue(e.target.value);
+            }}
+            className="my-3  w-full bg-brandDarkGray p-2  outline-none placeholder:text-gray-600"
+            rows="1"
+            placeholder="Rail Value Here, Default 3"
+          ></textarea>
+          <button
+            className="rounded-md bg-brandPink px-3 py-2 text-white"
+            onClick={() => {
+              setPlainText('');
+              setCipherText('');
+            }}
+          >
+            Clear All
+          </button>
+        </div>
+
+        <div class=" w-1/2 p-5">
+          <div class="font-sourceCodepro text-xl text-gray-500">
+            Cipher Text
+          </div>
+          <textarea
+            value={cipherText}
+            onChange={(e) => {
+              setCipherText(e.target.value);
+            }}
+            className="my-3  w-full bg-brandDarkGray p-2  outline-none placeholder:text-gray-600"
+            rows="10"
+            placeholder="Encrypted text will appear here"
+          ></textarea>
+          <button
+            className="mr-5 rounded-md bg-brandPink px-3 py-2 text-white"
+            onClick={() => {
+              setPlainText(decryptor(cipherText));
+            }}
+          >
+            Decrypt
+          </button>
+          <button
+            className="rounded-md bg-brandPink px-3 py-2 text-white"
+            onClick={() => {
+              setCipherText('');
+            }}
+          >
+            Clear Decrypt
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default railfence;
---
+++ pages/vigenere.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+
+const vigenere = () => {
+  const [plainText, setPlainText] = useState('');
+  const [cipherText, setCipherText] = useState('');
+  const [secretKey, setKey] = useState('key');
+
+  function vigenereCipher(message, key, Decrypt = false) {
+    message = message.trim().toLowerCase(); 
+    key = key.trim().toLowerCase();
+    key = key.replace(/[^a-zA-Z]/g, "");
+
+    let result = "";
+
+    if (Decrypt) {
+      for (let i = 0; i < message.length; i++) {
+        let m = message[i];
+        let k = key[i % key.length];
+  
+        if (m == ".") { result += " "; continue; }
+    
+        let shifted = String.fromCharCode(((m.charCodeAt(0) - k.charCodeAt(0)) + 26) % 26 + 97);
+    
+        result += shifted;
+      }
+    } else {
+      for (let i = 0; i < message.length; i++) {
+        let m = message[i];
+        let k = key[i % key.length];
+  
+        if (m == " ") { result += "."; continue; }
+  
+        let shifted = String.fromCharCode((m.charCodeAt(0) + k.charCodeAt(0) - 194) % 26 + 97);
+  
+        result += shifted;
+      }
+    }
+
+    return result;
+  }
+
+  return (
+    <div className="min-h-screen px-3 py-5 lg:px-20 lg:py-10">
+      <div class="w-full text-center text-6xl font-bold text-brandPink">
+        Vigenere Cipher
+      </div>
+      <div className="my-3 w-full text-left text-sm text-gray-600">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum
+        praesentium aliquam illum consequatur vitae inventore distinctio hic
+        nisi, sint molestias quam dignissimos et! Rem praesentium, soluta ipsum,
+        explicabo temporibus officiis iste minima odio iusto, quidem
+        necessitatibus quas. Quidem, ipsa nostrum!
+      </div>
+      <div class="flex w-full">
+        <div class=" w-1/2 p-5">
+          <div class="font-sourceCodepro text-xl text-gray-500">Plain Text</div>
+          <textarea
+            value={plainText}
+            onChange={(e) => {
+              setPlainText(e.target.value);
+            }}
+            placeholder="Enter text here"
+            className="my-3  w-full bg-brandDarkGray p-2  outline-none placeholder:text-gray-600"
+            rows="10"
+          ></textarea>
+          <button
+            className="mr-5 rounded-md bg-brandPink px-3 py-2 text-white "
+            onClick={() => {
+              setCipherText(vigenereCipher(plainText, secretKey));
+            }}
+          >
+            Encrypt
+          </button>
+          <button
+            className="rounded-md bg-brandPink px-3 py-2 text-white"
+            onClick={() => {
+              setPlainText('');
+            }}
+          >
+            Clear Encrypt
+          </button>
+        </div>
+
+        <div class=" w-1/2 p-5">
+          <div class="font-sourceCodepro text-xl text-gray-500">Key</div>
+          <textarea
+            // value={secretKey}
+            onChange={(e) => {
+              setKey(e.target.value);
+            }}
+            className="my-3  w-full bg-brandDarkGray p-2  outline-none placeholder:text-gray-600"
+            rows="1"
+            placeholder="Key Value Here, Default key"
+          ></textarea>
+          <button
+            className="rounded-md bg-brandPink px-3 py-2 text-white"
+            onClick={() => {
+              setPlainText('');
+              setCipherText('');
+            }}
+          >
+            Clear All
+          </button>
+        </div>
+
+        <div class=" w-1/2 p-5">
+          <div class="font-sourceCodepro text-xl text-gray-500">
+            Cipher Text
+          </div>
+          <textarea
+            value={cipherText}
+            onChange={(e) => {
+              setCipherText(e.target.value);
+            }}
+            className="my-3  w-full bg-brandDarkGray p-2  outline-none placeholder:text-gray-600"
+            rows="10"
+            placeholder="Encrypted text will appear here"
+          ></textarea>
+          <button
+            className="mr-5 rounded-md bg-brandPink px-3 py-2 text-white"
+            onClick={() => {
+              setPlainText(vigenereCipher(cipherText, secretKey, true));
+            }}
+          >
+            Decrypt
+          </button>
+          <button
+            className="rounded-md bg-brandPink px-3 py-2 text-white"
+            onClick={() => {
+              setCipherText('');
+            }}
+          >
+            Clear Decrypt
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default vigenere;
